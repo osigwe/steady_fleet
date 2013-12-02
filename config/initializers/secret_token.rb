d38f01c58ea889adad3fa71fcc7667f2b1b79642
Emeka <emeka.osigwe@aun.edu.ng>
2013-12-02 14:29:13 +0100

    Initial Commit: added rails project directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SteadyFleet::Application.config.secret_token = '918d529769037b625fdac1a159cfeb90f98f994914a7af51a6bcf361c03ce58dde238fa4a7cfccdb44baf7cf1940e11e7b4ee30e32d2da566675bec338eacb84'
